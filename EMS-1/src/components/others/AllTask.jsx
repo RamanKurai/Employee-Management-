f37f905b27a5f9187c9652ff173132ce994ad6db
Raman Kurai <ramankurai27@gmail.com>
2025-01-02 22:22:52 +0530

    changes done in others folder

--- EMS-1/src/components/others/AllTask.jsx
+++ EMS-1/src/components/others/AllTask.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import {AuthContext} from '../../context/AuthProvider'
+
 
 const AllTask = () => {
+
+  const [userData,setUserData] =  useContext(AuthContext)
+
   return (
     <div className='bg-[#1c1c1c] p-5 rounded mt-5'>
         <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
@@ -12,11 +17,11 @@ const AllTask = () => {
         </div>
         <div className=''>
              <div  className='border-2 border-emerald-500 mb-2 py-2 px-4 flex justify-between rounded'>
-            <h2 className='text-lg font-medium  w-1/5'></h2>
-            <h3 className='text-lg font-medium w-1/5 text-blue-400'></h3>
-            <h5 className='text-lg font-medium w-1/5 text-yellow-400'></h5>
-            <h5 className='text-lg font-medium w-1/5 text-white'></h5>
-            <h5 className='text-lg font-medium w-1/5 text-red-600'></h5>
+            <h2 className='text-lg font-medium  w-1/5'>{elem.firstName}</h2>
+            <h3 className='text-lg font-medium w-1/5 text-blue-400'>{elem.taskCounts.newTask}</h3>
+            <h5 className='text-lg font-medium w-1/5 text-yellow-400'>{elem.taskCounts.active}</h5>
+            <h5 className='text-lg font-medium w-1/5 text-white'>{elem.taskCounts.completed}</h5>
+            <h5 className='text-lg font-medium w-1/5 text-red-600'>{elem.taskCounts.failed}</h5>
         </div>
         </div>
         
